feb1f311dcacf223b68521f538e4fd654592cc0b
lupoj <lupoj@wit.edu>
2026-07-14 13:34:59 -0400

    Coded functionality for basic server-client chat model

---
+++ client.py
@@ -0,0 +1,58 @@
+import socket
+import threading
+
+def initialize_client():
+    host = "127.0.0.1"
+    port = 12345
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((host, port))
+        print("Connected to the server at", host, "on port", port)
+    except:
+        print("Failed to connect to the server at", host, "on port", port)
+        return
+    
+    username = input("Enter your username: ")
+
+    initial_message = username + " has joined the chat."
+    client.send(initial_message.encode())
+
+    client_thread = threading.Thread(target=receive_messages, args=(client,))
+    client_thread.start()
+
+    while True:
+
+        try:
+            message = input("Enter message: ")
+            if message.lower() == "exit":
+                break
+
+            if message.strip():
+                final_message = username + ": " + message + "\n"
+                client.send(final_message.encode())
+        except:
+            print("Error occurred while sending message.")
+            break
+
+        client.close()
+        print("Disconnected from the server.")
+
+def receive_messages(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            if not message:
+                print("Disconnected from the server.")
+                break
+
+            print(message.decode().strip())
+        except:
+            print("Error occurred while receiving message.")
+            break
+    
+    client.close()
+
+if __name__ == "__main__":
+    initialize_client()
